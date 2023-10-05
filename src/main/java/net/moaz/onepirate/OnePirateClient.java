eb3f9fbe8423e98ab9e4614c6109451bcc0342c3
Moaz Elseyoufi <moaz.elseyoufi14@gmail.com>
2023-10-05 21:58:02 +1100

    git commit -m "first commit"

---
+++ src/main/java/net/moaz/onepirate/OnePirateClient.java
@@ -0,0 +1,10 @@
+package net.moaz.onepirate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OnePirateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
